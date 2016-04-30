27251b02fbc4c7c0bdf3812b4181a0c198c64c1e
Jerome <marhic.jerome@gmail.com>
2016-04-30 19:53:59 +0200

    Using Unicorn

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # config valid only for current version of Capistrano
 lock '3.5.0'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'bets'
+set :repo_url, 'git@github.com:MarhicJeromeGIT/bets.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
@@ -24,7 +24,7 @@ set :repo_url, 'git@example.com:me/my_repo.git'
 # set :pty, true
 
 # Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 
 # Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system')
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,7 +2,7 @@
 # ======================
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
-server 'ec2-54-194-61-209.eu-west-1.compute.amazonaws.com/', user: 'ubuntu', roles: %w{app db web}
+server 'ec2-54-194-61-209.eu-west-1.compute.amazonaws.com', user: 'ubuntu', roles: %w{app db web}
 
 # server 'example.com', user: 'deploy', roles: %w{app db web}, my_property: :my_value
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
