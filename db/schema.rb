ac2c6543c20307d18c8116c0661de2d68369906f
Jerome <marhic.jerome@gmail.com>
2016-05-10 20:28:18 +0200

    Add ISO to countries

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160501181412) do
+ActiveRecord::Schema.define(version: 20160510182441) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20160501181412) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "poule"
+    t.string   "iso"
   end
 
   create_table "events", force: :cascade do |t|
